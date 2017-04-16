eb3078cbf15c4151359a6cf35de7c2934fedf0d7
Jeremy Fan <jeremyfan_14@live.com>
2017-04-17 00:06:44 +0800

    blur fix

--- 2017/script.js
+++ 2017/script.js
@@ -1,12 +1,15 @@
 document.querySelector('.icon-picture').onclick = function() {
 	var bg = document.querySelector('.bg');
+	var cover = document.querySelector('.cover');
 	var content = document.querySelector('.content');
 
 	if (bg.className === 'bg') {
 		bg.classList.add('blur')
+		cover.classList.add('shadow');
 		content.style.opacity = '1';
 	} else {
 		bg.classList.remove('blur');
+		cover.classList.remove('shadow');
 		content.style.opacity = '0';
 	}
 }
